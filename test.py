62c99f131bfec4c337790d61e9128282c3c645e5
zjyz2024 <zjyz2024@proton.me>
2025-08-14 08:16:42 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,20 @@
 import os
 import random
 import time
-
 from playwright.sync_api import sync_playwright
 
 EMAIL = os.getenv("EMAIL")
 PASSWORD = os.getenv("PASSWORD")
 TWOFA = os.getenv("TWOFA", "")
 
+URL = os.getenv("URL")
+
 PROXY = None  # 可设置为 "socks5://127.0.0.1:1080" 或 None
 
-URL = os.getenv("URL", "")
-
-
-
-
-
-
-
-# # 下面执行你的自动购买逻辑
 def run():
     with sync_playwright() as p:
         browser = p.chromium.launch(
-            headless=True,
+            headless=True, # True=无窗口 False=有窗口 
             proxy={"server": PROXY} if PROXY else None
         )
         context = browser.new_context()
@@ -45,11 +37,19 @@ def run():
         # 等待登录后跳转（如 dashboard 或首页）
         page.wait_for_load_state("networkidle")
 
+        # 判断是否需要取消账户保护
+        email_input = page.query_selector('input.form-control.maxwidth-auth#email')
+        if email_input:
+            print("检测到账户保护，正在尝试取消...")
+            email_input.fill(EMAIL)
+            page.click('button#reactive')
+            page.wait_for_load_state("networkidle")
+            print("✅ 已取消账户保护。")
+
+        # 自动签到
         el = page.query_selector('#checkin')
         if el:
             el.click()
-        else:
-            pass  # 没找到就跳过，不打印也不报错
 
         # 点击“套餐购买”链接
         page.click("a:has-text('套餐购买')")
@@ -57,25 +57,35 @@ def run():
         # 等待跳转完成
         page.wait_for_load_state("networkidle")
 
-        # 点击购买按钮（套餐ID为8）
-        # 3 days 80
-        # 1 days 90
+        # 点击购买按钮
+        # 3 days ID=8
+        # 1 days ID=9
         page.wait_for_selector('a[onclick="buy(\'8\',0)"]', timeout=5000)
         page.click('a[onclick="buy(\'8\',0)"]')
 
-        # ✅ 等待弹出确认按钮加载并点击“确定”
+        # 等待弹出确认按钮加载并点击“确定”
         page.wait_for_selector('#coupon_input', timeout=5000)
         page.click('#coupon_input')
 
-        # ✅ 第二步弹窗：点击 order_input 确定按钮
+        # 点击再次确定按钮
         page.wait_for_selector('#order_input', timeout=5000)
         page.click('#order_input')
-        print("已点击第二个确认按钮（order_input）")
 
+        # 等待购买完成
         page.wait_for_timeout(10000)
+
+        print("✅ 任务完成！")
+
         browser.close()
 
-wait_minutes = random.randint(0, 10)
-print(f"随机等待 {wait_minutes} seconds再开始执行任务")
-time.sleep(wait_minutes)
+def random_delay():
+    random_seconds = random.randint(0, 30)
+    time.sleep(random_seconds)
+    print("✅ 已随机休眠{}秒！".format(random_seconds))
+
+random_delay()
 run()
+
+
+# <input class="form-control maxwidth-auth" id="email" type="text">
+# <button id="reactive" type="submit" class="btn btn-block btn-brand waves-attach waves-light waves-effect">取消账户保护</button>
